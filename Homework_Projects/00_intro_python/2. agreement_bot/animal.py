3dde2c1630d335335e517a5765df9fff9368b57c
Ektakhatri <ekaterina870643@gmail.com>
2025-06-12 14:55:03 +0500

    Delete Homework_Projects directory

--- Homework_Projects/00_intro_python/2. agreement_bot/animal.py
+++
@@ -1,9 +0,0 @@
-#Program 2: Agreement bot
-
-def main():
-    animal = input("What's your favorite animal? ")
-    
-    print(f"My favorite animal is also {animal}!")
-
-if __name__ == "__main__":
-    main()
